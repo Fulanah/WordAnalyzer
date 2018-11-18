5c13a2b41487315181225f33e48738ba28f364f4
Fulanah <alarva36@yahoo.com>
2018-11-18 22:54:51 +0800

    Update App.js

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import btnAnalyzer from './btnAnalayzer';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,34 +17,36 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+//type Props = {};
+export default class App extends Component{
+
+  constructor(){
+    super();
+    this.state = {
+      word: '',
+      con: 0,
+      vow: 0,
+      char: 0
+    }
+
+  }
+
+  updateWord(){
+    this.setState({char: String.split().length}, ()=>
+    {
+
+
+    })
+  }  
+  
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <TextInput placeholder = 'Word:' onChangeText = {(word) => this.setState({word})}/>
+        <btnAnalyzer onPress = {() => this.updateWord()}/>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
