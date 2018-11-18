dbd5d3d22f8384129f643b1462ab35fd9501bc06
Fulanah <alarva36@yahoo.com>
2018-11-18 23:32:44 +0800

    Create Analayzer.js

---
+++ Analayzer.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+
+const Analyzer = (props) => {
+
+    return(
+        <View>
+            
+        </View>
+
+    );
+}
+
+
+const styles = StyleSheet.create ({
+
+    design:{
+        alignItems: 'center',
+        backgroundColor: '#DDDDDD',
+        padding: 10,
+        textAlign: 'center',
+        fontSize: 20
+    },
+
+    container: {
+        backgroundColor: '#ededed',
+        color: 'brown',
+        fontWeight: 'bold',
+        fontFamily: 'Impact',
+        fontSize: 20
+        }
+
+});
+
+export {Analyzer, styles};
